7c3d4d4c921b3f290e5d0a3533e22ac732b25cf0
Sergio Illán <illan2sergio@gmail.com>
2021-11-30 20:28:44 +0100

    Finishing login ui

--- Catstagram/Controller/Authentication/LoginController.swift
+++ Catstagram/Controller/Authentication/LoginController.swift
@@ -31,7 +31,58 @@ class LoginController: UIViewController {
     }()
     
     
+    private let passwordTexyField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.keyboardType = .emailAddress
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        tf.setHeight(50)
+        tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        
+        tf.isSecureTextEntry = true
+        return tf
+    }()
     
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemPurple
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),  .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Forgot your password?  ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        
+        attributedTitle.append(NSAttributedString(string: "Get help signing in.", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),  .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Don´t have an acount?  ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        
+        attributedTitle.append(NSAttributedString(string: "Sing Up", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
     
     //MARK: - Lifecycle
     
@@ -60,6 +111,17 @@ class LoginController: UIViewController {
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 60, width: 110)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTexyField, passwordTexyField,loginButton, forgotPasswordButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
     
     
